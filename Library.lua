644b9ea09e3231dd08f75d9bf4c2ab0eafdad117
secret <132377436+ATrainz@users.noreply.github.com>
2025-02-08 02:09:32 +1000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -444,7 +444,7 @@ end
 
 local FetchIcons, Icons = pcall(function()
 	return loadstring(
-		game:HttpGet("https://raw.githubusercontent.com/deividcomsono/lucide-roblox-direct/refs/heads/main/source.lua")
+		game:HttpGet("https://raw.githubusercontent.com/ATrainz/lucide-roblox-direct/refs/heads/main/source.lua")
 	)()
 end)
 function Library:GetIcon(IconName: string)
